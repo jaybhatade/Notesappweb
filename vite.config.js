545745693b582dd40362152947c772f10c286600
jaybhatade <jaybhatade7@gmail.com>
2024-08-11 16:37:26 +0530

    netlify

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3306", // Replace with your backend server URL
+        target: "https://noteswebapp7.netlify.app/", // Replace with your backend server URL
         changeOrigin: true,
         secure: false,
       },
